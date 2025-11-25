cd0cb6d4c7d843e80a28a50a2c2c7df6febfe646
Rishirish <pradhanhrishit09@gmail.com>
2025-11-25 19:13:55 +0530

    RTE API added

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -44,7 +44,7 @@ function RTE({name, control, label, defaultValue=""}) {
               content_style:
                 "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
             }}
-            onEditorChange={ onChange }
+            onEditorChange={onChange}
           />
         )}
       />
